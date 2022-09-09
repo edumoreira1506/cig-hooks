a4e249c5d83ac3ec3760170b1a5cb9dbba1124b1
edumoreira1506 <contato@eduardoem.com.br>
2022-09-09 18:06:08 -0300

    Add useRefetch hook

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as useLocalStorage } from './hooks/useLocalStorage';
 export { default as useDebouncedEffect } from './hooks/useDebouncedEffect';
 export { default as useInfiniteScroll } from './hooks/useInfiniteScroll';
+export * from './hooks/useRefetch';
